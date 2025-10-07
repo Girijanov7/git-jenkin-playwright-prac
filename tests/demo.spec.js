f340d554371f52aef25f90ce6cb0c1c615434e6f
Laks-tech <nishasingh493@gmail.com>
2025-10-07 11:44:52 +0530

    Update demo.spec.js
    i changed please review now

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -10,5 +10,5 @@ test("test 3", async ({ page }) => {
   await page.goto("https://playwright.dev/");
 });
 test ("folder1"), async ({page})=>{
-  awaait page.goto("myntra.com")
+  await page.goto("https://www.myntra.com")
 });
